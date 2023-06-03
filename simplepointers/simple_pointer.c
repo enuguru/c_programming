fb4e620242a8ecf0abaa40a0ca76de4d047ca5b7
enuguru <guruforsure@gmail.com>
2023-06-03 16:41:04 +0530

    all files

--- simplepointers/simple_pointer.c
+++ simplepointers/simple_pointer.c
@@ -3,7 +3,8 @@
 
 int main()
 {
-  int x =90; int *ptr = &x;
+  int x =90; 
+  int *ptr = &x;
   printf("The size of the integer is %lu bytes \n",sizeof(x));
   printf("The size of the pointer is %lu bytes \n",sizeof(ptr));
   return 0;
